c2717e8bc2bdd21b7b0a51c516a2cc99e4eb554c
xx1805xx <95584034+xx1805xx@users.noreply.github.com>
2024-05-21 18:15:35 +0200

    Add files via upload

---
+++ Foundation/FPersistentManager.php
@@ -0,0 +1,49 @@
+<?php
+
+class FPersistentManager{
+
+    private static $instance = null;
+
+    public static function getInstance(){
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    //END SINGLETON
+
+    public static function createObj($obj){
+        $class = "F" . substr(get_class($obj), 1);
+        $result = call_user_func([$class, "createObject"], $obj);
+        return $result;
+    }
+
+    public function retrieveObj($entity, $id){
+        $class = "F" . substr($entity,1);
+        $result = call_user_func([$class, "retrieveObject"], $id);
+        return $result;
+    }
+
+    public function updateObj($obj){
+        $class = "F" . substr(get_class($obj), 1);
+        $result = call_user_func([$class, "updateObject"], $obj);
+        return $result;
+    }
+
+    public function deleteObj($obj){
+        $class = "F" . substr(get_class($obj), 1);
+        $result = call_user_func([$class, "deleteObject"], $obj);
+        return $result;
+    }
+
+    //END CRUD
+
+
+    public static function verifyUserEmail($email){
+        $result = FUser::verify('email', $email);
+        return $result;
+    }
+}
+
+
