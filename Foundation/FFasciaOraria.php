f2ca2c9ca06e35d8486eca93649228cb04623094
Andrea <andrealuca.disimone@gmail.com>
2024-05-27 12:25:15 +0200

    metodi Ffasciaoraria implementati

--- Foundation/FFasciaOraria.php
+++ Foundation/FFasciaOraria.php
@@ -8,7 +8,7 @@ class FFasciaOraria{
     /** valori della tabella */
     private static $values="(:NULL,:data,:ora_inizio,:Calendario,:Appuntamento)";  //per l'autoincrement si fa così
 
-    private static $key = "idFasciaOraria";
+    private static $key = "IdFasciaOraria";
     /** costruttore */
     public function __construct() { }
 
@@ -60,8 +60,13 @@ class FFasciaOraria{
             $orario = array();
             for($i = 0; $i < count($queryResult); $i++){
                 $fasciaoraria = new EFasciaoraria($queryResult[$i]['data'],$queryResult[$i]['ora_inizio']);
-                $fasciaoraria->setIdFasciaOraria($queryResult[$i]['idFasciaoraria']);
-              
+                $fasciaoraria->setIdFasciaOraria($queryResult[$i]['idFasciaoraria']);  //PER LA PK AUTOINCREMENT
+                //come si mette il calendario?
+                //DA TESTARE
+                $calendario = FCalendario::getcalendariofromid($queryResult[$i]['Calendario']);  //il campo calendario è proprio l'id
+                $fasciaoraria->setCalendario($calendario);
+
+                //ispirazione presa da FReport
                 $orario[] = $fasciaoraria;
             }
             return $orario;   //ARRAY DELLE FASCE ORARIE LOADDATE
@@ -82,99 +87,28 @@ class FFasciaOraria{
         }
     }
 
-    
 
-
-
-
-
-
-
-
-
-
-
-    /**
-    * Permette la load sul db avendo come parametro di ricerca un campo passato in input alla funzione
-    * @param $id campo da confrontare per trovare l'oggetto
-    * @return object $fascor fascia oraria
-    */
-    public static function loadByField($field, $id){
-        $fascor = null;
-        $db = FDatabase::getInstance();
-        $result = $db->loadDB(static::getClass(), $field, $id);
-        $rows_number = $db->interestedRows(static::getClass(), $field, $id);
-        if(($result != null) && ($rows_number == 1)) {
-            $fascor = new EFasciaoraria($result['data'], $result['ora_inizio'], $result['Calendario'], $result['Appuntamento']);
-                $fascor->setId($result['IdFasciaOraria']);
-        }
-        else {
-            if(($result != null) && ($rows_number > 1)){
-                $fascor = array();
-                for($i = 0; $i < count($result); $i++){
-                    $fascor[] = new EFasciaoraria($result[$i]['data'], $result[$i]['ora_inizio'], $result[$i]['Calendario'], $result[$i]['Appuntamento']);
-                    $fascor[$i]->setId($result[$i]['IdFasciaOraria']);
-                }
+    //CON QUESTO UPDATEIAMO LE FASCE ORARIE (SERVIREBBE ANCHE UNA DELETE, LE MODIFICHE NON HANNO TROPPO SENSO)
+    public static function salvafasciaoraria($fasciaoraria){
+            $saveFasciaOraria = FEntityManagerSQL::getInstance()->saveObject(self::getClass(), $fasciaoraria);
+            if($saveFasciaOraria !== null){
+                return $saveFasciaOraria;
+            }else{
+                return false;
             }
-        }
-        return $fascor;
     }
 
     /**
-     * Metodo che permette il saalavtaggio di una fascia oraria nel db
-     * @param $fascor fascia oraria da salvare
-     * @return $id della Fascia Oraria salvata
+     * QUESTO SERVE PER CANCELLARE UNA FASCIA ORARIA con La sua PK COME ARGOMENTO(EVENTUALE CAMBIO DI DISPONIBILITà DI UN MEDICO)
+     * POTREBBE ESSERE MODIFICATO IN MODO DA DARE IN INPUT DIRETTAMENTE LA FASCIA
      */
-    public static function store(EFasciaoraria $fascor) {
-        $db = FDatabase::getInstance();
-        $id = $db->storeDB(static::getClass(), $fascor);
-        if($id) 
-            return $id;
-        else 
-            return null;
-    }
-    
-    /** 
-    * Funzione che permette di verificare se esiste un Luogo nel database
-    * @param  $id valore della riga di cui si vuo erificare l'esistenza
-    * @param  string $field colonna su ci eseguire la verifica
-    * @return bool $ris
-    */
-    /*
-    public static function exist($field, $id) {
-        $ris = false;
-        $db = FDatabase::getInstance();
-        $result = $db->existDB(static::getClass(), $field, $id);
-        if($result!=null) 
-            $ris = true;
-        return $ris;
-    }
-*/
-    /**
-     * Restituisce tutti i nomi dei luoghi presenti sul db
-     * @param $id campo da confrontare per trovare l'oggetto
-     * @return array di nomi di luoghi
-     */
-    /*
-    public static function loadNames($input){
-        $place = null;
-        $db = FDatabase::getInstance();
-        list ($result, $rows_number)=$db->ricercaLuogo($input);
-        if(($result != null) && ($rows_number == 1)) {
-            $place = ($result['name']);
+    public static function eliminafasciaoraria($IdFascia_oraria){        
+        $eliminaFasciaOraria = FEntityManagerSQL::getInstance()->deleteObjInDb(self::getTable(), "IdFasciaOraria", $IdFascia_oraria);
+        if($eliminaFasciaOraria !== null){
+            return $eliminaFasciaOraria;
+        }else{
+            return false;
         }
-        else {
-            if(($result != null) && ($rows_number > 1)){
-                $place = array();
-                for($i = 0; $i < count($result); $i++){
-                    $place[] = ($result[$i]['name']);
-                }
-            }
-        }
-        return $place;
     }
-*/
 
 }
-
-?>
