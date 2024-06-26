293e7b50ede95644049816bc30315ddc24f17eae
Di-Simone-dev <166904984+Di-Simone-dev@users.noreply.github.com>
2024-06-26 12:18:30 +0200

    Add files via upload

---
+++ Entity/EAppuntamento.php
@@ -0,0 +1,65 @@
+<?php
+
+class EAppuntamento
+{
+    private $IdAppuntamento;
+
+    private $costo; //per conservare la storia integra se il medico cambia il suo costo
+    
+    private $paziente;  //FK BISOGNA CAPIRE SE METTERE L'ID O NO 
+
+    private $fascia_oraria;   //FK
+
+    private static $entity = EAppuntamento::class;
+    //costruttore
+    public function __construct($costo)
+    {
+        //$this->$IdAppuntamento=$IdAppuntamento; PK
+        $this->costo=$costo;
+        //$this->$paziente=$paziente; FK
+        //$this->$fascia_oraria=$fascia_oraria; FK
+
+    }
+    //metodi get e set 
+    public function getIdAppuntamento()
+    {
+        return $this->IdAppuntamento;
+    }
+
+    public function setIdAppuntamento($IdAppuntamento)
+    {
+        $this->IdAppuntamento = $IdAppuntamento;
+    }
+
+    
+    public function getCosto()
+    {
+        return $this->costo;
+    }
+
+    public function setCosto($costo)
+    {
+        $this->stato = $costo;
+    }
+    
+
+    public function getPaziente()
+    {
+        return $this->paziente;
+    }
+
+    public function setpaziente($paziente)
+    {
+        $this->stato = $paziente;
+    }
+    public function getFasciaoraria()
+    {
+        return $this->paziente;
+    }
+
+    public function setFasciaoraria($IdFascia_oraria)
+    {
+        $this->fascia_oraria = $IdFascia_oraria;
+    }
+
+}
