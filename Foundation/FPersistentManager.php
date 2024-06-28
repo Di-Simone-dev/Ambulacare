adbd4081a3288ff7f6a816c2c6eb78a4203000fe
Andrea <andrealuca.disimone@gmail.com>
2024-06-28 22:03:57 +0200

    fix pers

--- Foundation/FPersistentManager.php
+++ Foundation/FPersistentManager.php
@@ -39,6 +39,7 @@ class FPersistentManager{
     public static function retrieveObj($class, $id){
        
         $foundClass = "F" . substr($class, 0);
+        var_dump($foundClass);
         $staticMethod = "getObj";    //il metodo della classe si deve chiamare così
 
         $result = call_user_func([$foundClass, $staticMethod], $id);
